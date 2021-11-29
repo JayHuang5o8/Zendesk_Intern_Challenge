f81eda269171860e87ed63fc3e4bd499ec86790c
jayhuang <shuang362@wisc.edu>
2021-11-28 16:39:19 -0800

    Add displaying a single ticket feature

--- TicketViewer.py
+++ TicketViewer.py
@@ -6,6 +6,17 @@ class TicketViewer:
     def __init__(self):
         pass
 
+    def execute_cmd(self, cmd: str):
+        '''
+        The function to handle the curl command. In this function, handle exceptions
+        Return:
+            curl result
+        '''
+        ret = subprocess.run(cmd, shell=True, capture_output=True)
+        # if res.returncode == 0:
+        res = json.loads(ret.stdout)
+        return res
+
     # ready to define other behaviors
 
     def login(self,  domain, username, password):
@@ -21,10 +32,8 @@ class TicketViewer:
         self.password = str(password)
         cmd = "curl https://{0}.zendesk.com/api/v2/tickets/count.json -u {1}:{2}".format(
             domain, username, password)
-        # not deal with error situation
-        ret = subprocess.run(cmd, shell=True, capture_output=True)
-        # if res.returncode == 0:
-        res = json.loads(ret.stdout)
+
+        res = self.execute_cmd(cmd)
         if 'error' in res:
             return -1
         else:
@@ -33,13 +42,20 @@ class TicketViewer:
             # other wrong
             return -1
 
-    # 0. need user to provide workspace domain, username and password
     # 1. fetch API to get tickets
     # 2. during the fetching process, handle the API unavaliable situation
 
-    def fetch(self) -> list:
+    def fetch_signle_ticket(self, ticket_id: int) -> dict:
+        cmd = "curl https://{0}.zendesk.com/api/v2/tickets/{3}.json -u {1}:{2}".format(
+            self.domain, self.username, self.password, ticket_id)
+        res = self.execute_cmd(cmd)
+        if 'error' in res:
+            return None
+        return res
+
+    def fetch_range_tickets(self, start: int, end: int) -> list:
         '''
-        Fetching tickets from ticket API. If API is not avaliable, return empty list
+        Fetching tickets from start id to end id. 
         Return:
             The list of tickets. If error, return empty list
         '''
@@ -50,18 +66,3 @@ class TicketViewer:
             return []
 
     # 3. need to display
-
-    def get_API(self) -> str:
-        '''
-        eturn the ticket API
-        '''
-        pass
-
-    def set_APT(self, api: str) -> int:
-        '''
-        change the ticket api
-        Return:
-            1: success
-            0: failed
-        '''
-        pass
--- ViewDriver.py
+++ ViewDriver.py
@@ -28,10 +28,11 @@ class commandLineDisplay:
                 failed_cnt += 1
                 print("Sorry, your provided credentials do not in our records.")
                 if failed_cnt > 2:
-                    print("Please sign up first or verify your credentials.")
+                    print(
+                        "Please sign up first or verify your credentials, then try again.")
                     exit()
                 print(
-                    "Please try again (failed_times : {0})".format(failed_cnt))
+                    "Please try again (failed_times : {0})\n".format(failed_cnt))
             if cnt >= 0:
                 print("Welcome to {0}!".format(self.domain))
                 print("Currently you have {0} tickets.".format(cnt))
@@ -50,7 +51,59 @@ class commandLineDisplay:
         exit()
 
     def menu(self):
+        PROMPT = "\n\
+        Select view options:\n\
+            * Press 1 to view all tickets\n\
+            * Press 2 to view a particular\n\
+            * Type 'quit' to exit\n"
+        while True:
+            cmd = input(PROMPT)
+            if cmd == 1:
+                # TODO display each 25 tickets
+                pass
+            elif cmd == 2:
+
+                pass
+            elif cmd == 'quit':
+                return
+            else:
+                print("Your input is invalid, please re-select.")
+
+    def display_header(self):
+        '''
+        Set the output format for displaying ticket
+        ticket id, status, subject, how long, due_at, assignee_id
+        '''
+        HEADER = "{0:<10}  {1:<10}  {2:<25}  {3:<20}  {4:<20}  {5:<15}\n".format(
+            "Ticket_id", "Status", "Ticket_subject", "Created_date", "Due_at", "assignee_id")
+        return HEADER
+
+    def transform(self, ticket: dict):
+        '''
+        transform a signle ticket to a standard output format
+        '''
+        res = "{0:<10}  {1:<10}  {2:<25}  {3:<20}  {4:<20}  {5:<15}\n".format(
+            ticket.get('id'), ticket.get('status'), ticket.get('subject')[:25],
+            str(ticket.get('created_at')), str(ticket.get('due_at')), str(ticket.get('assignee_id')))
+        return res
+
+    def display_signle_ticket(self, ticket_id: int):
+        '''
+        Displaying a signle ticket according to output format
+        '''
+        ticket = self.tv.fetch_signle_ticket(ticket_id)
+        if ticket:
+            res = ''
+            res += self.display_header()
+            res += self.transform(ticket)
+            return res
+        else:
+            print("Ticket id {0} does not exit.".format(ticket_id))
+
+    def display_range_tickets(self, start: int, end: int):
+        # for a range of tickets, max range is 25 tickets
         pass
+
 
         # first check count.json to see how many tickets it has, then select ticket range
 if __name__ == "__main__":
